8b2d4599668a8b1d0ba1d9e18c62fb0ca78fa638
user.name <2065136374@qq.com>
2024-02-18 12:59:11 +0000

    oct4class task is ok next work: transfer to octa task and organize the code

---
+++ log/log.md
@@ -0,0 +1,68 @@
+## notice  
+1. 做好镜像管理、数据管理和记录，记得version ctrl和备份   
+2. 
+
+## code explaination 
+- 用datalist（存储样本文件地址的list）来维护数据集
+- 测试2d 2d多通道 3d vit  
+- 该implement是将同一层的od os进行水平concat  
+
+## env
+```
+docker run -p 49440:22 -it --rm -d --ipc host  --gpus 1  --mount type=bind,source=/media/oct,target=/code  chen
+```
+```
+ssh -p 17639 chen@3.26.210.83
+```
+## log
+### 1
+- 完成了datalist方式维护数据加载列表
+func（create datalist）写在了train.py，之后挪到utilize中
+### 2
+- od os 水平concate
+- 写了balance（aug） func，之后挪到utilize中
+- 2d vit/2.5d vit  不work,能正常跑通，但net学不到特征
+- 之后尝试使用别的数据集
+### 3 2024.2.18
+- 引入一个新的数据集进行测试（datasets/paultimothymooney/），oct的4分类task
+- 整理一下代码，做一下注释
+- oct4class在ce上很不稳定，换成bce with logits
+- 448 bce bs=64 depth=16 heads=10 adam (bce_oct4_class.pt)
+![Alt text](image.png)
+![Alt text](image-1.png)
+- model换成vgg16， 也不理想
+![Alt text](image-2.png)
+- load covid的预训练试一试  
+TODO: 加载失败，先改成strict false 跑一下试试看  
+能work a bit, loss稍微有些改善(work_oct4class_epoch14.pt, 继续收敛，已删除) rmk: val精度波动比较大，写一下网络保存策略（val acc），然后增加一下bs，稍微降一点lr继续跑
+![Alt text](image-6.png)
+![Alt text](image-7.png)
+Next:
+- 想办法解决load问题
+- 学习一下torch的权重修改
+- oct4class跑到90的acc，再去做octa task
+- 学习一下[pretrain](https://www.analyticsvidhya.com/blog/2020/08/top-4-pre-trained-models-for-image-classification-with-python-code/)
+- 学习一下[Train a Vision Transformer on small datasets](https://keras.io/examples/vision/vit_small_ds/)
+- load covid-vit 2d/3d 在oct上，看能否work然后发一下结果
+- 记得修改一下val的间隔
+- 标化记录一下实验结果
+# 4 2024.2.19
+- vgg加载pretrain之后的效果很不错（下一步规范下代码）(存了一个接近收敛的版本，work_oct4class_vgg.pt)
+oct4class_vgg_1 log
+![Alt text](image-9.png)
+- vit收敛速度太慢（比vgg差很多），bs还可以继续加，总之先试图跑到收敛（搞定，后续测试Resnet2d以及3d）
+- 如果当前收敛完效果不如vgg的话，vit加载一个google的预训练（也是224）然后进行训练
+- 目前vit还有收敛趋势（oct4class_1 -> 2），lr貌似太大，减半接着跑，感觉bs可能太大影响收敛，试一下小bs  
+![Alt text](image-10.png)
+减小bs没用，再改回去
+![Alt text](image-11.png)
+- 想试一下大的lr看下效果（1e4），保存在oct4class_biglr
+- TODO: 用一个字典来配置模型的训练设置
+- lr大了之后效果很好，收敛更快，并且效果更好
+![Alt text](image-12.png)
+和5e-6比较
+![Alt text](image-13.png)
+基本收敛了，save 在oct4_class_biglr, val_acc=0.96  
+- 尝试用上面这个来训练一下octa data  
+    - 把三通道os、od、os
+    - transform修改一下
---
+++ train.py
@@ -0,0 +1,343 @@
+import glob
+from itertools import chain
+import os
+import argparse
+import random
+import zipfile
+import gc
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from torch.optim.lr_scheduler import StepLR
+from torch.utils.data import DataLoader, Dataset
+from torchvision import datasets, transforms
+from tqdm.notebook import tqdm
+from linformer import Linformer
+import torchvision.transforms.functional as TF
+from torch.autograd import Variable
+import torch.nn.functional as F
+import io
+import random
+from sklearn.model_selection import train_test_split
+
+#from vit_pytorch.efficient import ViT
+#from vit_pytorch.efficient import ViT
+from vit_pytorch.vit import ViT
+from vit_pytorch.deepvit import DeepViT
+import wandb
+device = torch.device("cuda:2")
+wandb.init(project="my-project")
+parser = argparse.ArgumentParser(description='Pytorch COVID-ViT 2D/3D Training')
+#parser.add_argument('--save', default='/code/covid_ckpts/oct4class_pretrained/', type=str, help='model save path')
+parser.add_argument('--save', default='/code/covid_ckpts/oct4class_biglr/', type=str, help='model save path')
+parser.add_argument('--best', default=0, help='best accuracy')
+args = parser.parse_args()
+if not os.path.exists(args.save):
+    os.makedirs(args.save)
+
+
+def init_weights(m):
+    if type(m) == nn.Linear:
+        torch.nn.init.uniform_(m.weight)
+
+def train_epoch(epochs, train_loader, model, criterion, optimizer, scheduler, eval_loader=None):
+    for epoch in range(epochs):
+        epoch_loss = 0
+        epoch_accuracy = 0
+        batch_cnt = 0
+        for data, label in tqdm(train_loader):
+            data = data.to(torch.float32)
+            label = label
+            allocated = torch.cuda.memory_allocated(torch.device(device))
+            output = model(data)
+            print(f'Output: {output.shape}', f'Label: {label.shape}')
+            # loss = 7*criterion(output, label) + 3*nn.MSELoss()(torch.argmax(output, dim=1).to(torch.float32), label) 
+            loss = criterion(output, label)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+            wandb.log({'epoch': epoch, 
+                       'train_loss': loss, 
+                       'train_avg_loss': loss/ len(train_loader),
+                       'lr': optimizer.param_groups[0]['lr'],
+                       #'train_mse': nn.MSELoss()(torch.argmax(output, dim=1).to(torch.float32), label),
+                       'train_acc': (torch.argmax(output, dim=1) == torch.argmax(label, dim=1)).sum()/len(label)})
+
+            epoch_loss += loss / len(train_loader)
+            # DEBUG
+            print(f"Allocated: {allocated}, Mixloss: {(loss/ len(train_loader)):.5f} ")
+            batch_cnt += 1
+            # validating between every n batch
+            if(batch_cnt%12 == 0):
+                eval(eval_loader, model, criterion)
+            # TODO save the checkpoint every batch 
+            torch.save(model.state_dict(), 'xg_vit_model_covid_2d.pt')
+    
+        print(f"Epoch {epoch+1}: | epoch-loss-avg: {epoch_loss:.5f}")
+        # inference on whole train data
+        wandb.log({'Val_whole_epoch': eval(train_loader, model, criterion)})
+        if(epoch%100 == 0):
+            torch.save(model.state_dict(), f'/code/covid_ckpts/epoch{epoch}.pt')
+
+def eval(eval_loader, model, criterion):
+    with torch.no_grad():
+        epoch_loss = 0
+        val_acc = 0
+        batch_cnt = 0
+        for data, label in eval_loader:
+            data = data.to(device).to(torch.float32)
+            label = label.to(device)
+
+            output = model(data)
+            loss = criterion(output, label)
+            epoch_loss += loss / len(eval_loader)
+            # print(f"Label: {label.tolist()}")
+            # outputs = [torch.argmax(output[i]).item() for i in range(eval_loader.batch_size)]
+            # print(f"Output: {outputs}")
+            val_acc += (torch.argmax(output, dim=1) == torch.argmax(label, dim=1)).sum()/len(label)
+            batch_cnt += 1
+        print(f"Evaluation loss: {epoch_loss:.5f}")    
+        mean_batch_acc = val_acc / batch_cnt
+        wandb.log({'Val_loss': epoch_loss, 
+                   # 'Val_mse': nn.MSELoss()(torch.argmax(output, dim=1).to(torch.float32), label), 
+                   'val_acc': mean_batch_acc})
+        if(mean_batch_acc > args.best):
+            args.best = mean_batch_acc
+            torch.save(model.state_dict(), args.save + f'val_acc{mean_batch_acc}.pt')
+        return mean_batch_acc
+
+def predict(model):
+    
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Resize((224, 224)),
+        ])
+    datas = CovidDataset(train_list, label_list, transform=transform)
+    with torch.no_grad():
+        mse = 0
+        for i in range(len(train_list)):
+            data = datas.__getitem__(i)[0]
+            data = data.to(torch.float32).unsqueeze(0).to(device)
+            label = datas.__getitem__(i)[1]
+            output = model(data)
+            output = torch.argmax(output[0])
+            print(f"Predicted: {output}, Actual: {label}")
+            mse += (output - label)**2
+        print(f"MSE: {mse/len(train_list)}")
+
+def get_dataOCT(path):
+    '''
+    获得oct_kaggle的datalist和labels
+    --oct
+        --train(path)
+            -class1
+            -class2
+    '''
+    cls = os.listdir(path) # get the class name(folders name)
+    pair = {i:cls.index(i) for i in cls}
+    datalist = glob.glob(path+'**/*.jpeg',recursive=True)
+    datalistOCT = []
+    for i in cls:
+        files = os.listdir(os.path.join(path,i))
+        files = [os.path.join(path,i,j) for j in files]
+        datalistOCT.extend(random.sample(files,8616))
+    random.shuffle(datalistOCT)
+    print('oct_kaggle sample count: ',len(datalistOCT))
+    labels = [pair[(i.split('/')[-2])] for i in datalistOCT]
+    return datalistOCT, labels
+
+get_dataOCT('/code/oct_kaggle/OCT2017/train/')
+def balance(train_list, label_list):
+    ''' 用于类别平衡 '''
+    # 去重
+    all = list(set(label_list))
+    times = [label_list.count(i) for i in all]
+    new_train=train_list
+    new_label=label_list
+    for i in all:
+        tmp_train = []
+        for t in range(len(label_list)):
+            if label_list[t]==i:
+                tmp_train.append(train_list[t])
+        for j in range(max(times)-label_list.count(i)):
+            new_train.append(tmp_train[random.randint(0,len(tmp_train)-1)]) 
+            new_label.append(i)
+    return new_train, new_label
+
+class CovidDataset(Dataset):
+    def __init__(self, file_list, labels, transform=None):
+        self.file_list = file_list
+        self.transform = transform
+        self.labels = labels
+
+    def __len__(self):
+        self.filelength = len(self.file_list)
+        return self.filelength
+
+    def __getitem__(self, idx):
+        
+        #img_3d = nib.load(self.file_list[idx]).get_fdata()
+        img_3d_os = np.load(self.file_list[idx][0].replace('.nii.gz','.npy').replace('/code/images-with-labels/', '/code/images_npy/'), mmap_mode='r')
+        img_3d_od = np.load(self.file_list[idx][0].replace('.nii.gz','.npy').replace('/code/images-with-labels/', '/code/images_npy/'), mmap_mode='r')
+        # randomly select a 2D slice
+        # z = np.random.randint(0,img_3d_os.shape[2]-1)
+        # select 4 2D slice
+        z_max = img_3d_os.shape[2]-1
+        z = [5,int(z_max/4), int(z_max/2), z_max-5]
+
+        imgs = [np.expand_dims(np.concatenate((img_3d_os[:, :, i],img_3d_od[:, :, i]),axis=0), axis=2) for i in z]
+        imgs = np.concatenate(imgs, axis=2)
+
+        #print('2d-img-shape:',imgs.shape)
+        img_transformed = self.transform(imgs)
+        #print('2d-img-shape=',img_transformed.size(),type(img_transformed))
+        return img_transformed.to(device), torch.from_numpy(np.expand_dims(self.labels[idx]-85, axis=0)).to(device)[0]
+    
+class oct_kaggleDataset(Dataset):
+    def __init__(self, file_list, labels, transform=None):
+        self.file_list = file_list
+        self.transform = transform
+        self.labels = labels
+
+    def __len__(self):
+        self.filelength = len(self.file_list)
+        return self.filelength
+
+    def __getitem__(self, idx):
+        img = Image.open(self.file_list[idx])
+        img_transformed = self.transform(img)
+        cls = self.labels[idx]
+        # cls to one-hot
+        label = torch.zeros(4)
+        label[cls] = 1
+        # 复制成3通道
+        img_transformed = torch.cat([img_transformed, img_transformed, img_transformed], dim=0)
+        return img_transformed.to(device), label.to(device)
+def timing(s):
+    # 从文件名中提取时间， 用于排序
+    s = s.replace('-', '_').split('_')
+    return s[-6] + s[-8].rjust(2, '0') + s[-7].rjust(2, '0') + s[-5].rjust(2, '0') + s[-4].rjust(2, '0') + s[-3].rjust(2, '0')
+
+
+# Prepare the dataset
+
+transform = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Resize((224, 224)),
+        #transforms.RandomHorizontalFlip(),  # Randomly flip the image horizontally
+        #transforms.RandomVerticalFlip(),  # Randomly flip the image vertically
+        # transforms.RandomCrop((448, 448)),  # Randomly crop the image to size (224, 224)
+    ]
+)
+
+# trian_list contain file path of the images
+train_list = glob.glob('/code/images-with-labels/**/*.nii.gz', recursive=True)
+# 一个patient一个文件夹
+sample_folder = set(['/'.join(i.split('/')[:-1]) for i in train_list])
+
+# 没有区分OS OD
+# train_list = [os.path.join(i,sorted(os.listdir(i), key=timing)[-1]) for i in sample_folder]
+# 区分OS OD
+train_list = []
+
+for i in sample_folder:
+    os_list = [i for i in os.listdir(i) if i.split('_')[-2] == 'OS']
+    od_list = [i for i in os.listdir(i) if i.split('_')[-2] == 'OD']
+    train_list.append([os.path.join(i,sorted(os_list, key=timing)[-1]), os.path.join(i,sorted(od_list, key=timing)[-1])])
+ex = pd.read_excel('/code/Sleep-results.xlsx')
+# load label from train_list and excel
+label_list = [(ex[ex.iloc[:,0] == i[0].split('/')[-2]])['sat_avg'].iloc[0] for i in train_list[:]]
+train_list, label_list = balance(train_list, label_list)
+# 划分数据集
+train_list, test_list, train_label, test_label = train_test_split(train_list, label_list, 
+                                                                 test_size=0.15, 
+                                                                 random_state=33)
+# octa dataset
+# train_data = CovidDataset(train_list, train_label, transform=transform)
+# valid_data = CovidDataset(test_list,test_label, transform=transform)
+# test_data = CovidDataset(test_list, label_list,transform=transform)
+
+# oct_kaggledataset
+train_list, label_list = get_dataOCT('/code/oct_kaggle/OCT2017/train/')
+train_list, test_list, train_label, test_label = train_test_split(train_list, label_list, 
+                                                                 test_size=0.15, 
+                                                                 random_state=33)
+train_data = oct_kaggleDataset(train_list, train_label, transform=transform)
+valid_data = oct_kaggleDataset(test_list,test_label, transform=transform)
+test_data = oct_kaggleDataset(test_list, label_list,transform=transform)
+batch_size = 320
+train_loader = DataLoader(dataset = train_data, batch_size=batch_size, shuffle=True)
+valid_loader = DataLoader(dataset = valid_data, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(dataset = test_data, batch_size=batch_size, shuffle=True)
+
+# determine the model
+device = torch.device(device)
+efficient_transformer = Linformer(
+    dim=1024,
+    seq_len=49+1,  # 7x7 patches + 1 cls-token
+    depth=10,
+    heads=8,
+    k=64
+)
+# model = ViT(
+#     dim=1024,
+#     image_size=448,
+#     patch_size=32,
+#     num_classes=4,
+#     depth=16,
+#     heads=10,
+#     # transformer=efficient_transformer,
+#     mlp_dim=1024,
+#     channels=1,
+#     dropout=0.1,
+# ).to(device)
+# official pretrian
+model = ViT(
+    dim=1024,
+    image_size=224,
+    patch_size=32,
+    num_classes=2,
+    depth=12,
+    heads=8,
+    mlp_dim=1024,
+    # transformer=efficient_transformer,
+    channels=3,
+).to(device)
+# wts = torch.load('/code/chen/pretrain/net.pt')
+# model.load_state_dict(wts, strict=False)
+model.mlp_head = nn.Linear(1024,4)
+model.to(device)
+
+
+
+
+# Resume from last run
+# model.apply(init_weights)
+pretrained_net = torch.load('xg_vit_model_covid_2d.pt')
+model.load_state_dict(pretrained_net)
+
+
+# loss function
+criterion = nn.MSELoss()
+criterion = nn.BCEWithLogitsLoss()
+# optimizer
+optimizer = optim.Adam(model.parameters(), lr=1e-4)
+# optimizer = optim.SGD(model.parameters(), lr=1e-6, momentum=0.)
+# scheduler
+# scheduler = StepLR(optimizer, step_size=20, gamma=0.6)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=150, eta_min=0)
+
+# train the model
+print(f'Sample size: {len(train_data)}, Eval size: {len(valid_data)}')
+train_epoch(1000, train_loader, model, criterion, optimizer, scheduler, valid_loader)
+pretrained_net = torch.load('/code/xg_vit_model_covid_2d.pt')
+model.load_state_dict(pretrained_net)
+predict(model)
